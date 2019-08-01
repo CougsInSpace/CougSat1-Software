517104b413fe72e511b94d4090cfc5a93b425075
Bradley Davis <mathxbikes@comcast.net>
2019-08-01 01:00:43 -0700

    Created custom board for Comms

--- CougSat1-Comms/src/main.cpp
+++ CougSat1-Comms/src/main.cpp
@@ -1,36 +1,15 @@
-/******************************************************************************
- * Copyright (c) 2018 by Cougs in Space - Washington State University         *
- * Cougs in Space website: cis.vcea.wsu.edu                                   *
- *                                                                            *
- * This file is a part of flight and/or ground software for Cougs in Space's  *
- * satellites. This file is proprietary and confidential.                     *
- * Unauthorized copying of this file, via any medium is strictly prohibited.  *
- ******************************************************************************/
-/**
- * @file main.cpp
- * @author Bradley Davis
- * @date 6 Mar 2018
- * @brief Starts the IHU software
- *
- * Initializes IHU object and starts the eventQueue
- */
-
 #include <mbed.h>
 
-#include <ADC/AD7291.h>
-#include <ADC/AD7689.h>
-#include <Thermistor.h>
-
-SPI spi(SPI_MOSI, SPI_MISO, SPI_SCK);
+DigitalOut statusLED(LED1);
 
 /**
  * Program start routine
  * @return error code
  */
 int main(void) {
-  AD7689 adc(spi, PA_10);
-  ADC *  pADC = &adc;
-  Thermistor t(adc, ADCChannel_t::CM_00, 0.0, 0.0);
-  pADC->getReferenceVoltage();
+  while (true) {
+    statusLED = !statusLED;
+    wait_ms(500);
+  }
   return MBED_SUCCESS;
 }
