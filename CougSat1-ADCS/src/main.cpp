987b983594fd56816629e97d916e1b0db0528700
warior4356 <warior4356@yahoo.com>
2019-10-07 18:59:15 -0700

    Priority Queue and Communication Framework restored

--- CougSat1-ADCS/src/main.cpp
+++ CougSat1-ADCS/src/main.cpp
@@ -1,16 +1,9 @@
 #include <CISConsole.h>
 #include <mbed.h>
-#include "CISBoard/PinNames.h"
-//#include "CISBoard/PeripheralPins.c"
-#include "CISBoard/PeripheralNames.h"
-
-
-/*
 #include "ADCSObjects.h"
 #include "Configuration.h"
-//#include "events/Events.h"
 #include "interfaces/CDH.h"
-*/
+
 /**
  * @brief Initializes the all of the subclasses of the ADCS
  *
@@ -22,25 +15,6 @@
  * @return error code
  */
 int main(void) {
-  /*mbed_error_status_t result = initialize();
-  if (result) {
-    ERROR("ADCS", "Failed to initialize: 0x%02X", result);
-  }
-  result = run();
-  if (result) {
-    ERROR("ADCS", "Failed to run: 0x%02X", result);
-  }
-  return MBED_SUCCESS;*/
-    printf("Outside Main!\r\n");
-    DEBUG("ADCS", "Outside Main!");
-    //DigitalOut led1(PinName(5));
-    while (true)
-    {
-        printf("Process Main!\r\n");
-        DEBUG("ADCS", "Process Main!");
-        //led1 = !led1;
-        wait(1);
-    }
-  //ADCS adcs;
-  //adcs.initThread();  
+  ADCS adcs;
+  adcs.initThread();  
 }
