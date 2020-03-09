cba9a5078a43219ab86bfdd3921e497ca7fe7b3d
Odeysiuss Tuon <odeysiuss.tuon@wsu.edu>
2020-03-08 17:22:44 -0700

    Fixed bugs and tested code

--- CISLibrary/drivers/T6713/T6713.cpp
+++ CISLibrary/drivers/T6713/T6713.cpp
@@ -1,32 +1,30 @@
 #include "T6713.h"
 
 /**
- * @brief Construct a new T6713 object
- *
- * @param i2c  The I2C with which the driver works
- * @param addr The slave address of the sensor on the i2c
- */
-T6713::T6713(I2C & i2c, uint8_t addr) {
-  this->i2c  = i2c;
-  this->addr = addr;
-}
-
-/**
- * @brief Destroy the T6173 object.
+ * @brief Destroy the T6713 object.
  * As of now, the destructor does not need anything.
  *
  */
-~T6713::T6173() {}
+T6713::~T6713() {}
 
 /**
  * @brief Gets the PPM (CO2 level) reading of the sensor
  *
- * @return int
+ * @param outValue Reference to the int variable to be changed to the PPM value
+ * @return mbed_error_status_t MBED_SUCCESS if the reading was successful,
+ * otherwise the error status of the reading
  */
-int T6173::readPPM() {
+mbed_error_status_t T6713::readPPM(int & ppmOut) {
   char data[4];
-  i2c.write(this->addr, READ_CO2_COMMAND, 5) / ;
-  thread_sleep_for(2000);
-  i2c.read(this->addr, data, 4);
-  return (((data[2] & 0x3F) << 8) | data[3]);
+
+  if (this->i2c.write(this->addr, READ_CO2_COMMAND, 5)) {
+    return MBED_ERROR_WRITE_FAILED;
+  }
+
+  if (this->i2c.read(this->addr, data, 4)) {
+    return MBED_ERROR_READ_FAILED;
+  }
+
+  ppmOut = (((data[2] & 0x3F) << 8) | data[3]);
+  return MBED_SUCCESS;
 }
--- CISLibrary/drivers/T6713/T6713.h
+++ CISLibrary/drivers/T6713/T6713.h
@@ -1,43 +1,52 @@
 #ifndef _LIBRARY_DRIVER_T6713_H_
 #define _LIBRARY_DRIVER_T6713_H_
 
+// See
+// http://www.co2meters.com/Documentation/Manuals/Manual-AMP-0002-T6713-Sensor.pdf
+// for more detailed documentation of the sensor itself
+
 #include <mbed.h>
 
-// T6173 CO2 Sensor i2C Address
-const uint8_t DEFAULT_T6713_SLAVE_ADDRESS 0x15 << 1;
+// T6713 CO2 Sensor I2C default slave address
+const uint8_t DEFAULT_T6713_SLAVE_ADDRESS = 0x15 << 1;
 // Same command found in the T67XX CO2 Sensor Module documentation, see pg. 29
-static const uint8_t READ_CO2_COMMAND = {0x04, 0x13, 0x8B, 0x00, 0x01};
+static const char READ_CO2_COMMAND[5] = {0x04, 0x13, 0x8B, 0x00, 0x01};
 
 class T6713 {
 public:
-  T6713(const T6173 &) = delete;
-  T6173 & operator=(const T6173 &) = delete;
+  T6713(const T6713 &) = delete;
+  T6713 & operator=(const T6713 &) = delete;
 
   /**
    * @brief Construct a new T6713 object
    *
    * @param i2c  The I2C with which the driver works
-   * @param addr The slave address of the sensor on the i2c
+   * @param addr The slave address of the sensor on the i2c, defaults to
+   * DEFAULT_T6713_SLAVE_ADDRESS
    */
-  T6713(I2C & i2c, uint8_t addr = DEFAULT_T6713_SLAVE_ADDRESS);
+  T6713(I2C & i2c, uint8_t addr = DEFAULT_T6713_SLAVE_ADDRESS) :
+    i2c(i2c), addr(addr) {};
 
-  ~T6173();
+  ~T6713();
 
   /**
-   * @brief Reads the current PPM (CO2 level) that the sensor reads
+   * @brief Gets the PPM (CO2 level) reading of the sensor
    *
-   * @return int The PPM reading of the sensor
+   * @param outValue Reference to the int variable to be changed to the PPM
+   * value
+   * @return mbed_error_status_t MBED_SUCCESS if the reading was successful,
+   * otherwise the error status of the reading
    */
-  int readPPM();
+  mbed_error_status_t readPPM(int & outValue);
 
 private:
   /**
    * @brief The I2C with which the driver works to read the sensor values
    *
    */
-  I2C i2c;
+  I2C & i2c;
   /**
-   * @brief The slave address of the sensor on which the I2C should read/write
+   * @brief The slave address of the sensor to which the I2C should read/write
    *
    */
   uint8_t addr;
