66404f1188ae98e446e0396193aebcd7c0514ce8
warior4356 <warior4356@yahoo.com>
2020-03-08 17:00:43 -0700

    Adds file changes required to make nucleo work

--- CougSat1-ADCS/src/CISBoard/PinNames.h
+++ CougSat1-ADCS/src/CISBoard/PinNames.h
@@ -171,10 +171,22 @@ typedef enum {
   // Not connected
   NC = (int)0xFFFFFFFF,
 
-  STDIO_UART_TX = NC,
-  STDIO_UART_RX = NC,
-  USBTX         = STDIO_UART_TX,
-  USBRX         = STDIO_UART_RX,
+// STDIO for console print
+#ifdef MBED_CONF_TARGET_STDIO_UART_TX
+  STDIO_UART_TX = MBED_CONF_TARGET_STDIO_UART_TX,
+#else
+  STDIO_UART_TX = PA_2,
+#endif
+#ifdef MBED_CONF_TARGET_STDIO_UART_RX
+  STDIO_UART_RX = MBED_CONF_TARGET_STDIO_UART_RX,
+#else
+  STDIO_UART_RX = PA_3,
+#endif
+
+  // STDIO_UART_TX = NC,
+  // STDIO_UART_RX = NC,
+  USBTX = STDIO_UART_TX,
+  USBRX = STDIO_UART_RX,
 
   /*** ADCS HW V1.0 ***/
   LED1 = PD_2,
