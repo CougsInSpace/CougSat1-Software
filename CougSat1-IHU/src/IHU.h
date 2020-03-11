034d9a937fb1a0f010cb455d4493af73c235ba2d
Jonathan <jonathan.manly@wsu.edu>
2020-03-11 16:57:42 -0700

    Fixed a typo

--- CougSat1-IHU/src/IHU.h
+++ CougSat1-IHU/src/IHU.h
@@ -45,7 +45,7 @@ namespace IHU
         /// Adds a one off event to the queue.
         /// @param objs IHUObjects to use for the function call.
         /// @param f Function pointer of function to call.
-        /// @param args Variatic arguments to be forwarded to the call function.
+        /// @param args Variadic arguments to be forwarded to the call function.
         /// @return ID of event in queue.
         template <typename F, typename... Args>
         int addEvent(IHUObjects &objs, F f, Args &&... args)
@@ -57,7 +57,7 @@ namespace IHU
         /// @param objs IHUObjects to use for the function call.
         /// @param obj The object who's function you want to call.
         /// @param method Function pointer of function to call.
-        /// @param args Variatic arguments to be forwarded to the call function.
+        /// @param args Variadic arguments to be forwarded to the call function.
         /// @return ID of event in queue.
         template <typename T, typename U, typename... Args>
         int addEvent(IHUObjects &objs, T *obj, U (T::*method)(Args...),
@@ -70,7 +70,7 @@ namespace IHU
         /// Adds a recurring event to the queue.
         /// @param objs IHUObjects to use for the function call.
         /// @param f Function pointer of function to call.
-        /// @param args Variatic arguments to be forwarded to the call function.
+        /// @param args Variadic arguments to be forwarded to the call function.
         /// @param ms Number of milliseconds to wait before executing function
         /// call.
         /// @return ID of event in queue.
@@ -86,7 +86,7 @@ namespace IHU
         /// @param objs IHUObjects to use for the function call.
         /// @param obj The object who's function you want to call.
         /// @param method Function pointer of function to call.
-        /// @param args Variatic arguments to be forwarded to the call function.
+        /// @param args Variadic arguments to be forwarded to the call function.
         /// @return ID of event in queue.
         template <typename T, typename U, typename... Args>
         int addEventRecurring(int32_t ms, IHUObjects &objs, T *obj,
