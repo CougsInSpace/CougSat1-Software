200a8b738816e1bb934789051572ad4d182cbe21
Vaibhav Nandha <nandhavaibhav9@gmail.com>
2021-02-24 19:34:32 -0800

    encoding Frame
    created structure for encoding frames

--- CISLibrary/components/communications/Frame.cpp
+++ CISLibrary/components/communications/Frame.cpp
@@ -33,13 +33,100 @@ bool Frame::isDone() {
   return false;
 }
 
+
+size_t Frame::amendCode(uint8_t *buf, uint8_t code6, uint8_t code4, bool start){
+static uint8_t bitOffset;
+if (start){
+  bitOffset = 0;
+}
+switch (bitOffset) 
+{
+case 0: 
+  *buf = (code6<<2)|(code4>>2);
+  *(buf+1) = (code4<<6);
+  bitOffset = 2;
+  return 1;
+
+case 2: 
+  *buf|= code6;
+  *(buf+1) = (code4<<4);
+  bitOffset = 4;
+  return 1;
+
+case 4:
+  *buf|= (code6>>2);
+  *(buf+1) = (code6<<6)|(code4<<2);
+  bitOffset = 6;
+  return 1;
+
+case 6:
+  *buf|= (code6>>4);
+  *(buf+1) = (code6<<4)|(code4);
+  bitOffset = 0;
+  return 2;//fully filled buf and 1 baby!;
+
+default:
+  break;
+}
+}
+
 /**
  * @brief Get the next byte to transmit
  *
- * @return uint8_t
+ * @return uint8_t next 8 bits to transmit or END_OF_FILE if no more to transmit
  */
 uint8_t Frame::getNextTXByte() {
-  return -1;
+  static uint8_t encodedFrameBuffer [MAX_FRAME_ENCODED];
+  static size_t encodedFrameLength = 0;
+  static size_t transmitOffset = 0;
+
+
+
+  if (!encoded){
+    bool negativeRD = true;
+    encodedFrameLength = 0;
+    uint8_t *currentBuf = encodedFrameBuffer;
+    // generate a bitstream
+
+    // preamble 
+    currentBuf += amendCode(currentBuf, CommandCodes6::PREAMBLE_NEG,CommandCodes4::PREAMBLE_NEG, true);
+    currentBuf += amendCode(currentBuf, CommandCodes6::PREAMBLE_NEG,CommandCodes4::PREAMBLE_NEG);
+    currentBuf += amendCode(currentBuf, CommandCodes6::PREAMBLE_NEG,CommandCodes4::PREAMBLE_NEG);
+    currentBuf += amendCode(currentBuf, CommandCodes6::PREAMBLE_NEG,CommandCodes4::PREAMBLE_NEG);
+    currentBuf += amendCode(currentBuf, CommandCodes6::PREAMBLE_NEG,CommandCodes4::PREAMBLE_NEG);
+    currentBuf += amendCode(currentBuf, CommandCodes6::PREAMBLE_NEG,CommandCodes4::PREAMBLE_NEG);
+    currentBuf += amendCode(currentBuf, CommandCodes6::PREAMBLE_NEG,CommandCodes4::PREAMBLE_NEG);
+
+    // start
+    currentBuf += amendCode(currentBuf, CommandCodes6::START_NEG,CommandCodes4::START_NEG);
+
+
+    // payload
+    // iterate throught the payload
+    // convert payload byte to code
+    // first 5 bits - lookup table
+    // last 3 bits - lookup table  note edge case (alternate code)
+    // if not even RD, flip negativeRD
+
+    // 8, 6+2, 4+4, 6+2, 8,
+    
+    // encode frame into buffer
+
+    // set encoded frame length
+
+
+    // reset transmit offset
+    transmitOffset = 0;
+    encoded = true;
+  }
+
+  if (encodedFrameLength == transmitOffset){
+    // reached the last code in the buffer
+    return END_OF_FILE;
+  }
+
+  return encodedFrameBuffer[transmitOffset++];
+
 }
 
 } // namespace Communications
--- CISLibrary/components/communications/Frame.h
+++ CISLibrary/components/communications/Frame.h
@@ -15,12 +15,110 @@ public:
   bool isDone();
 
   uint8_t getNextTXByte();
+  static const uint8_t END_OF_FILE = 0;
+
 
 private:
-  static const size_t MAX_PAYLOAD = 1024 + 2; // Packet + packet header
+  // Packet + packet header
+  static const size_t MAX_PAYLOAD = 1024 + 2; 
+  // Preamble + Start of Frame + Payload + CRC + End of Frame
+  static const size_t MAX_FRAME_DECODED = 7 + 1 + MAX_PAYLOAD + 2 + 1;
+  // Max Frame Decoded * 10 bits/code + 1 to round up
+  static const size_t MAX_FRAME_ENCODED = MAX_FRAME_DECODED * 10/8 + 1; 
+
+  // static const uint16_t START_CODE;
+
+struct Code {
+  uint8_t negativeCode;
+  uint8_t positiveCode;
+  bool evenRD;
+  // TODO consider bit packing
+};
+
+static const Code CODES_56[] = {
+  {0b100111, 0b011000, false},
+  {0b011101, 0b100010, false},
+  {0b101101, 0b010010, false},
+  {0b110001, 0b110001, true},
+  {0b110101, 0b001010, false},
+  {0b101001, 0b101001, true},
+  {0b011001, 0b101001, true},
+  {0b111000, 0b000111, false},
+  {0b111001, 0b000110, false},
+  {0b100101, 0b100101, true},
+  {0b010101, 0b010101, true},
+  {0b110100, 0b110100, true},
+  {0b001101, 0b110100, true},
+  {0b101100, 0b101100, true},
+  {0b011100, 0b011100, true},
+  {0b010111, 0b101000, false},
+  {0b011011, 0b100100, false},
+  {0b100011, 0b100011, true},
+  {0b010011, 0b010011, true},
+  {0b110010, 0b110010, true},
+  {0b001011, 0b001011, true},
+  {0b101010, 0b101010, true},
+  {0b011010, 0b011010, true},
+  {0b111010, 0b000101, false},
+  {0b110011, 0b001100, false},
+  {0b100110, 0b100110, true},
+  {0b010110, 0b100110, true},
+  {0b110110, 0b001001, false},
+  {0b001110, 0b001110, true},
+  {0b101110, 0b010001, false},
+  {0b011110, 0b100001, false},
+  {0b101011, 0b010100, false}
+};
+
+static const Code CODES_34[] = {
+  {0b1011, 0b0100, false},
+  {0b1001, 0b1001, true},
+  {0b0101, 0b0101, true},
+  {0b1100, 0b0011, false},
+  {0b1101, 0b0011, false},
+  {0b1010, 0b1010, true},
+  {0b0110, 0b0110, true},
+  {0b1110, 0b0001, false},
+  {0b0111, 0b1000, false}
+};
+
+static const enum class CommandCodes6: uint8_t{
+  PREAMBLE_NEG = 0b001111,
+  PREAMBLE_POS = 0b110000,
+
+  START_NEG = 0b111010,
+  START_POS = 0b000101,
+
+  END_NEG = 0b110110,
+  END_POS = 0b001001
+};
+
+static const enum class CommandCodes4: uint8_t{
+  PREAMBLE_NEG = 0b1010,
+  PREAMBLE_POS = 0b0101,
+
+  START_NEG = 0b1000,
+  START_POS = 0b0111,
+
+  END_NEG = 0b1000,
+  END_POS = 0b0111
+};
+
+size_t amendCode(uint8_t *buf, uint8_t code6, uint8_t code4, bool start = false);
+
+/*   enum class State: uint8_t{
+    START,
+    PAYLOAD_DATA,
+    CRC_1,
+    CRC_2,
+    END
+  };
+ */
 
   uint8_t payloadData[MAX_PAYLOAD];
   size_t  length = 0;
+
+  bool encoded = false;
 
   // TODO add CRC field
 };
