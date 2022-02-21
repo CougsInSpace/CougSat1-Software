4e29a90fcfc78f40e5b48875970c58eb95b046ed
Trav798 <68037803+Trav798@users.noreply.github.com>
2022-02-20 16:37:48 -0800

    Added vector functions head file

---
+++ CougSat1-ADCS/src/vectorFunctions.cpp
@@ -0,0 +1,1 @@
+#include "vectorFunctions.h"
---
+++ CougSat1-ADCS/src/vectorFunctions.h
@@ -0,0 +1,7 @@
+#ifndef vectorFunctions_H_
+#define vectorFunctions_H_
+
+#include "../../CISLibrary/eigen-master/Eigen/Dense"
+
+
+#endif /*vectorFunctions_H_*/
