3de6fda70796cc60f3f099e6aed6110faf0aae49
Alexander Shirk <alexander@truewheels.net>
2022-09-04 15:43:15 -0700

    basic filtering works

--- CougSat1-ADCS/docs/magnetic model conversion/json_file_filter.py
+++ CougSat1-ADCS/docs/magnetic model conversion/json_file_filter.py
@@ -9,16 +9,28 @@ files_to_process = ["igrfgridData_x.json", "igrfgridData_y.json", "igrfgridData_
 #list of json tags to keep 
 tags_to_keep = ["latitude", "longitude", "xcomponent"]
 
-def strip_unwanted_tags(d): #strips unwanted json tags, only includes desired tags 
-    for item in d["result"]:
-        print(item)
-        break
+def strip_unwanted_tags(d, tags): #strips unwanted json tags, only includes desired tags 
+    filtered_list = [] #the list which contains only the tags we want
+    for item in d["result"]: #d["result"] gives a list of dictionaries containing our data. We iterate through this list, so item is a dictionary of our data. 
+        temp_dict = {} #temporary dict which will only contain our desired tags
+        for tag in tags: #for each tag we want to keep 
+            if tag in item.keys(): #if we have the tag 
+                temp_dict[tag] = item[tag] #add the data from this tag into our temp dict 
+            else: #if we don't have the tag, show an error 
+                print("Error! Item does not contain tag: " + tag) 
+        filtered_list.append(temp_dict) #
+    return filtered_list #return the filtered list containing only the tags we want 
 
-
+def merge_tags(l1, l2): #merges tags from 2 different lists of dicts. Used to get the x, y, and z components all in one 
+    pass
 
 for file in files_to_process: #processes each file 
     f = open(file, "r")
     dict_from_file = json.loads(str(f.read())) #represents the json from the file as a python dictionary 
-    strip_unwanted_tags(dict_from_file)
     f.close()
+
+    filtered_list = strip_unwanted_tags(dict_from_file, tags_to_keep)
+
+
+
     break
