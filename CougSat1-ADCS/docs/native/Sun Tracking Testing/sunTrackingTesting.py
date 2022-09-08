83ec9012880688d2d6e3b6e09ed86e4c736cf8aa
Trav798 <68037803+Trav798@users.noreply.github.com>
2022-09-07 19:23:40 -0700

    Tested right ascension to longitude conversion

--- CougSat1-ADCS/docs/native/Sun Tracking Testing/sunTrackingTesting.py
+++ CougSat1-ADCS/docs/native/Sun Tracking Testing/sunTrackingTesting.py
@@ -5,25 +5,26 @@ import numpy as np
 import datetime as dt
 
 def main():
-    currenttime = dt.datetime(2022,9,20)
-    timeDelta = dt.timedelta(hours=1)
-    raList=[]
-    decList = []
-    for i in range(24*3):
-        raDec = astronomy.sun_ra_dec(currenttime)
-        newlong = astronomy.gmst(currenttime) - raDec[0]
-        if newlong > np.pi:
-            newlong -= 2*np.pi
-        raList.append(newlong)
-        decList.append(raDec[1])
-        currenttime += timeDelta
+    currenttime = dt.datetime(2023,3,20, hour=12,minute=9,second=4) # default minute 9
+    print(currenttime)
+    timeDelta = dt.timedelta(hours=23, minutes=56)
+    # raList=[]
+    # decList = []
+    # for i in range(24*3):
+    #     raDec = astronomy.sun_ra_dec(currenttime)
+    #     newlong = astronomy.gmst(currenttime) + raDec[0]
+    #     if newlong > np.pi:
+    #         newlong -= 2*np.pi
+    #     raList.append(newlong)
+    #     decList.append(raDec[1])
+    #     currenttime += timeDelta
 
-    x = np.array(range(24*3))
-    y = np.array(raList)
+    # x = np.array(range(24*3))
+    # y = np.array(raList)
 
-    plt.plot(x,np.rad2deg(y))
-    plt.show()
-
+    # plt.plot(x,np.rad2deg(y))
+    # plt.show()
+    print(astronomy.gmst(currenttime + timeDelta + timeDelta))
     #gmst = greenwich mean sidereal time , which should be used with right ascension to determine longitude
     #gmst is measured in radians
     #autumnal equinox is when right ascension resets, passing over the astronomical version of the international date line
@@ -31,5 +32,6 @@ def main():
     #print(currenttime)
     #print(astronomy.gmst(currenttime))
     #print(np.rad2deg(astronomy.gmst(currenttime)))
+
 if __name__ == "__main__":
     main()
