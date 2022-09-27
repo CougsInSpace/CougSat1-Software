4a3f9b94d9fd2a188936b96b1dca31cb6c23c88a
arydab1 <ary.dab@gmail.com>
2022-09-26 22:06:47 -0700

    Vector Functions Added--Still needs review+testing
    Functions still need to be tested/reviewed

--- CougSat1-ADCS/python to cpp conversions/vectorFunctionsCpp.cpp
+++ CougSat1-ADCS/python to cpp conversions/vectorFunctionsCpp.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include "../../CISLibrary/Eigen/Eigen_Repo/Eigen/Dense"
-#include "../../CISLibrary/Eigen/Eigen_Repo/Eigen/Geometry"
+#include "../../CISLibrary/Eigen/Dense"
+#include "../../CISLibrary/Eigen/Geometry"
 
 using Eigen::MatrixXd;
 using Eigen::Quaternion;
@@ -9,10 +9,18 @@ using namespace std;
 
 #define FLOAT_COMPARISON_EPSILON 0.00001 //value used for comparing floats to 0 
 
+
+
+
+
 bool floatCloseToZero(float f);
 Vector4f rodRotation(Vector3f a, Vector3f aT);
 Vector3f rotationVector(Vector3f a, Vector3f aT);
 float vecAngle(Vector3f u, Vector3f v);
+Vector3f saturate(Vector3f v, float min, float max);
+float vecOrthoAngle(Vector3f u, Vector3f v, Vector3f ax);
+Vector3f planeProject(Vector3f v, Vector3f n);
+Vector3f vectorProject(Vector3f v, Vector3f u);
 
 bool floatCloseToZero(float f) {
     return f < FLOAT_COMPARISON_EPSILON;
@@ -21,6 +29,53 @@ bool floatCloseToZero(float f) {
 
 
 int main() {
+
+//Saturate and torque2Dipole seem to be working fine, still requires testing
+
+
+
+//Saturate function test
+//test param Vector(5,10,15)
+//min:3 , max: 16
+
+Vector3f satTest(5,10,15);
+Vector3f satTest1(9,20,-4);
+Vector3f satTest2(-3, -4,9.5);
+Vector3f satTest3(20,30,-90);
+
+float min = 3;
+float max = 12; 
+
+ 
+Vector3f satReturn = saturate(satTest,min,max);
+cout << satReturn;  
+
+//findTorque test
+//test param: u(5,5,5) v(7,7,7) mag(3,2,1)
+Vector3f u(5,5,5); 
+Vector3f v(7,7,7);
+Vector3f mag(3,2,1);
+
+//findTorque(u,v,mag); 
+
+
+//applyRodRotation test
+//test param: v2(4,4,4) rod(4,4,4,50)
+
+Vector4f rod(4,4,4,50);
+Vector3f v2(4,4,4);
+
+//applyRodRotation(v2,rod);
+
+
+//torque2diople test
+//param: tordueDir(5,5,5) mag2(6,6,6)
+
+Vector3f torqueDir(5,5,5);
+Vector3f mag2(6,6,6);
+
+//torque2Dipole(torqueDir,mag2); 
+
 
     return 0;
 }
@@ -66,14 +121,72 @@ Vector3f rotationVector(Vector3f a, Vector3f aT) {
     return rotVec;
 }
 
+//TODO: untested, implementation needed, need to doulbe check 
+ Vector3f applyRodRotation(Vector3f v, Vector4f rod) {
+
+   Vector3f k = rod;
+
+    float theta = rod[3]; 
+
+     k = k/k.norm();  
+     v = v/v.norm(); 
+
+// add scalars 
+    Vector3f vT = (v * cos(theta)) + (k.cross(v) * sin(theta)) + (k * k.dot(v))*(1 - cos(theta));
+    return vT; 
+ } 
+
+//TODO: Untested
+Vector3f torque2Dipole(Vector3f torqueDir, Vector3f mag) {
+
+    Vector3f dipoleDir = mag.cross(torqueDir);
+
+    return dipoleDir;
+}
 
 
 
+//TODO: test functions, find thetaErrorVec
+Vector3f findTorque(Vector3f u, Vector3f v, Vector3f mag) {
+
+Vector3f w1 = u.cross(v); 
+
+Vector3f w2 = (u + v) / 2; 
+
+Vector3f norm = w1.cross(w2);
+Vector3f torque = (norm.cross(mag)); 
+
+float theta = vecOrthoAngle(u,v,torque);
+Vector3f rod((-1 * torque), theta);
+Vector3f vec = applyRodRotation(u,rod); 
+
+if (vecAngle(vec,v) < 0.00001){
+   torque = (-1 * torque);
+}
+
+return torque; 
+
+}
+
+
+//TODO: testing, sytax check
+Vector3f saturate(Vector3f v, float min, float max) {
+
+
+    for (int i = 0; i < v.size(); i++) {
+        if (v[i] > max || v[i] < min) {
+            v *= abs(max / v[i]);
+        }
+
+    }
+
+    return v; 
+}
 
 
 
-
-
+// these functions are already tested 
+//theta error
 float vecAngle(Vector3f u, Vector3f v) {
     float vn = v.norm();
     float un = u.norm();
@@ -81,3 +194,26 @@ float vecAngle(Vector3f u, Vector3f v) {
     float theta = acos(v.dot(u) / (vn * un));
     return theta;
 }
+
+float vecOrthoAngle(Vector3f u, Vector3f v, Vector3f ax) {
+    Vector3f uOrtho = planeProject(u,ax);
+    Vector3f vOrtho = planeProject(v,ax);
+
+    return vecAngle(uOrtho, vOrtho);
+}
+
+Vector3f planeProject(Vector3f v, Vector3f n) {
+    v.normalize();
+    n.normalize();
+    Vector3f vProj = vectorProject(n,v);
+    return v - vProj;
+}
+
+Vector3f vectorProject(Vector3f v, Vector3f u) {
+    // project u onto v
+    float vn = v.norm();
+    float un = u.norm();
+
+    return v * (v.dot(u) / (vn * un));
+}
+
--- CougSat1-ADCS/python to cpp conversions/vectorFunctionsPython(1).py
+++ CougSat1-ADCS/python to cpp conversions/vectorFunctionsPython(1).py
@@ -30,11 +30,48 @@ def main(): #just for testing
     # outputs for identical inputs. Test them separetly from rest of ADCS code
     # before putting them in the vectorFunctions.h file
 
-    #Example
-    x = np.array([1,2,3])
-    y = np.array([3,4,1])
 
-    print(rodRotation(x,y))
+
+    ##Saturate and torque2Dipole seem to be working fine, still requires testing
+
+
+  ##saturate tests
+  satTest1 = np.array([5,10,15])
+  satTest2 = np.array([9,20,-4])
+  satTest3 = np.array([-3,-4,9.5])
+  satTest4 = np.array([20,30,-90])
+
+  print(saturate(satTest1)) 
+  print(saturate(satTest2)) 
+  print(saturate(satTest3)) 
+  print(saturate(satTest4)) 
+
+  ##findTorque test
+  u = np.array([5,5,5])
+  v = np.array([7,7,7])
+  mag = np.array([3,2,1])
+
+  print(findTorque(u,v,mag))
+
+  ##applyRodRotation test
+  v2 = np.array([4,4,4])
+  rod = np.array([4,4,4,50])
+
+  print(applyRodRotation(v2,rod))
+
+  ## torque2Dipole test
+  tordqueDir = np.array([5,5,5])
+  mag2 = np.array([6,6,6])
+
+  print(torque2Dipole(tordqueDir,mag2))
+
+
+
+    #Example
+  x = np.array([1,2,3])
+  y = np.array([3,4,1])
+
+  print(rodRotation(x,y))
 
 
 def rodRotation(a: np.ndarray, aT: np.ndarray):
