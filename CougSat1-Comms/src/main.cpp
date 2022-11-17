fea09b99e3cb4b791aaeaffcd4928c3d41676e9d
Trav798 <68037803+Trav798@users.noreply.github.com>
2022-11-16 18:29:25 -0800

    Comms board testing

--- CougSat1-Comms/src/main.cpp
+++ CougSat1-Comms/src/main.cpp
@@ -1,79 +1,82 @@
 #include <mbed.h>
-#include <iostream>
+// #include <iostream>
 #include <CISConsole.h>
 
-#include <Synth/ADF4360.h>
+HAL_RCC_MCOConfig(PA_8, RCC_MC01SOURCE_SYSCLOCK, RCC_MCODIV_2);
+// #include <Synth/ADF4360.h>
 
-#include <Trig.h>
+// #include <Trig.h>
 
-DigitalOut statusLED(LED1, 1);
+// DigitalOut statusLED(LED1, 1);
 
-DigitalOut regEn5(REG_EN_5, 0); // goes to TPS61236P, steps up voltage while stepping down current
+// DigitalOut regEn5(REG_EN_5, 0); // goes to TPS61236P, steps up voltage while stepping down current
 
-DigitalOut pcMod3(PC_3V1_1, 0); // LP5907MFX-3.1, voltage regulator
-DigitalOut pcMod5(PC_5V0_1_N, 1); // INA199C1, current shunt monitor
-DigitalOut pcDemod(PC_3V1_2, 0);
+// DigitalOut pcMod3(PC_3V1_1, 0); // LP5907MFX-3.1, voltage regulator  
+// DigitalOut pcMod5(PC_5V0_1_N, 1); // INA199C1, current shunt monitor
+// DigitalOut pcDemod(PC_3V1_2, 0);
 
-DigitalOut antennaSwRX(ANTENNA_SW, 0); // goes directly to antenna
+// DigitalOut antennaSwRX(ANTENNA_SW, 0); // goes directly to antenna
 
-SPI     spi(SPI_MOSI, SPI_MISO, SPI_SCK); // Serial Peripheral Interface, similiar to I2C
-ADF4360 rfClkMod(spi, RFCLK_CS_MOD_N, 7, 20000000); // signal send oscillator?
-ADF4360 rfClkDemod(spi, RFCLK_CS_DEMOD_N, 7, 20000000); // signal receive oscillator?
+// SPI     spi(SPI_MOSI, SPI_MISO, SPI_SCK); // Serial Peripheral Interface, similiar to I2C
+// ADF4360 rfClkMod(spi, RFCLK_CS_MOD_N, 7, 20000000); // signal send oscillator?
+// ADF4360 rfClkDemod(spi, RFCLK_CS_DEMOD_N, 7, 20000000); // signal receive oscillator?
 
-PortOut    dacBus(PortE, 0xFFC8); // PortE enum to 4. 0xFFC8=1111111111001000 is a mask, decides which bits in the output will be used. Apparently used to control the signal. Is 32 bit?
-PortOut    dacClk(PortE, 0x0010); // 0x0010=00010000 just the clock pin?
-DigitalOut dacSleep(MODDAC_SLEEP, 0);
+// PortOut    dacBus(PortE, 0xFFC8); // PortE enum to 4. 0xFFC8=1111111111001000 is a mask, decides which bits in the output will be used. Apparently used to control the signal. Is 32 bit?
+// PortOut    dacClk(PortE, 0x0010); // 0x0010=00010000 just the clock pin?
+// DigitalOut dacSleep(MODDAC_SLEEP, 0);
 
 /**
  * Program start routine
  * @return error code
  */
 int main(void) {
-  // while(true) {
-  //   printf("hello\n");
-  //   cout << "hello2" << endl;
-  // }
-  regEn5 = 1;
-  wait_us(10e3);
-  pcMod3 = 1;
-  pcMod5 = 1;
-  wait_us(10e3);
-  rfClkMod.setFrequency(435000000);
-  wait_us(10e3);
-
-  dacSleep = 1;
-  wait_us(10);
-  dacSleep = 0;
-  wait_us(10e3);
-  // cout << "hello" << endl;
-
-  while (true) {
-    // printf("helloo2");
-    statusLED = !statusLED;
-    for (int i = 0; i < 138 * 360; ++i) {
-      // uint16_t codeI = uint16_t((LUT_SINE[i % TRIG_LUT_SIZE] + 1) / 2 * 1023
-      // + 0.5);
-
-      // uint16_t codeQ = uint16_t((LUT_SINE[(i + TRIG_LUT_COSINE) %
-      // TRIG_LUT_SIZE] + 1) / 2 * 1023 + 0.5);
-
-      // Setup and hold times met due to 80MHz => 12.5ns > 3ns
-      uint16_t codeI = LUT_SINE_10b[(i + 0) % TRIG_LUT_SIZE]; // fast way to do sine. Max value of Look-Up Table is 752, a 10 bit int
-      dacClk         = 0x00;
-      // The mask for PortOut dacBus is 1111111111001000. CodeI will be some 16 bit int representing sine. The last 6 bits of the mask are 0 with the exception of the one in the 4th position.
-      // The line of code before compares some int codeI in the binary form xxxxxxxxxxxxxxx with an or operator. The mask takes the first 10 positions of that int and keeps them, throws out the
-      // rest. The int is left shifted 6 to account for the first 6 places of the mask being 0 with the exception of the 4th, which is taken care of with the or 0x8 (the number 8, or 1000 in binary)
-      dacBus         = (codeI << 6) | 0x8; // bit math, compare 8 in binary (1000) to whatever codeI << 6 is. starts as 16 bit, shifted over to 22(?) bit
-      dacClk         = 0x10;
-      wait_us(5);
-
-      uint16_t codeQ = LUT_SINE_10b[(i + TRIG_LUT_COSINE) % TRIG_LUT_SIZE];//fast wat to do cosine?
-      dacClk         = 0x00;
-      dacBus         = (codeQ << 6) | 0x0;
-      dacClk         = 0x10;
-      wait_us(5);
-      // 1/(10us*360) = 277Hz
-    }
+  
+  // SWO_Channel * swo = SWOSingleton::Instance()->get();
+  while(true) {
+    LOG("str", "hellooo\r\n");
   }
-  return MBED_SUCCESS;
+
+//   regEn5 = 1;
+//   wait_us(10e3);
+//   pcMod3 = 1;
+//   pcMod5 = 1;
+//   wait_us(10e3);
+//   rfClkMod.setFrequency(435000000);
+//   wait_us(10e3);
+
+//   dacSleep = 1;
+//   wait_us(10);
+//   dacSleep = 0;
+//   wait_us(10e3);
+//   // cout << "hello" << endl;
+
+//   while (true) {
+//     // printf("helloo2");
+//     statusLED = !statusLED;
+//     for (int i = 0; i < 138 * 360; ++i) {
+//       // uint16_t codeI = uint16_t((LUT_SINE[i % TRIG_LUT_SIZE] + 1) / 2 * 1023
+//       // + 0.5);
+
+//       // uint16_t codeQ = uint16_t((LUT_SINE[(i + TRIG_LUT_COSINE) %
+//       // TRIG_LUT_SIZE] + 1) / 2 * 1023 + 0.5);
+
+//       // Setup and hold times met due to 80MHz => 12.5ns > 3ns
+//       uint16_t codeI = LUT_SINE_10b[(i + 0) % TRIG_LUT_SIZE]; // fast way to do sine. Max value of Look-Up Table is 752, a 10 bit int
+//       dacClk         = 0x00;
+//       // The mask for PortOut dacBus is 1111111111001000. CodeI will be some 16 bit int representing sine. The last 6 bits of the mask are 0 with the exception of the one in the 4th position.
+//       // The line of code before compares some int codeI in the binary form xxxxxxxxxxxxxxx with an or operator. The mask takes the first 10 positions of that int and keeps them, throws out the
+//       // rest. The int is left shifted 6 to account for the first 6 places of the mask being 0 with the exception of the 4th, which is taken care of with the or 0x8 (the number 8, or 1000 in binary)
+//       dacBus         = (codeI << 6) | 0x8; // bit math, compare 8 in binary (1000) to whatever codeI << 6 is. starts as 16 bit, shifted over to 22(?) bit
+//       dacClk         = 0x10;
+//       wait_us(5);
+
+//       uint16_t codeQ = LUT_SINE_10b[(i + TRIG_LUT_COSINE) % TRIG_LUT_SIZE];//fast wat to do cosine?
+//       dacClk         = 0x00;
+//       dacBus         = (codeQ << 6) | 0x0;
+//       dacClk         = 0x10;
+//       wait_us(5);
+//       // 1/(10us*360) = 277Hz
+//     }
+//   }
+//   return MBED_SUCCESS;
 }
---
+++ CougSat1-Comms/test/Hackrf/fmradio.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: Not titled yet
+# GNU Radio version: 3.10.4.0
+
+from packaging.version import Version as StrictVersion
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print("Warning: failed to XInitThreads()")
+
+from PyQt5 import Qt
+from gnuradio import qtgui
+from gnuradio.filter import firdes
+import sip
+from gnuradio import analog
+from gnuradio import audio
+from gnuradio import filter
+from gnuradio import gr
+from gnuradio.fft import window
+import sys
+import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+import osmosdr
+import time
+
+
+
+from gnuradio import qtgui
+
+class fmradio(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Not titled yet")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "fmradio")
+
+        try:
+            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+                self.restoreGeometry(self.settings.value("geometry").toByteArray())
+            else:
+                self.restoreGeometry(self.settings.value("geometry"))
+        except:
+            pass
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 4e6
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
+                interpolation=12,
+                decimation=5,
+                taps=[],
+                fractional_bw=0)
+        self.qtgui_sink_x_0 = qtgui.sink_c(
+            1024, #fftsize
+            window.WIN_BLACKMAN_hARRIS, #wintype
+            435e6, #fc
+            20e6, #bw
+            "", #name
+            True, #plotfreq
+            True, #plotwaterfall
+            True, #plottime
+            True, #plotconst
+            None # parent
+        )
+        self.qtgui_sink_x_0.set_update_time(1.0/5)
+        self._qtgui_sink_x_0_win = sip.wrapinstance(self.qtgui_sink_x_0.qwidget(), Qt.QWidget)
+
+        self.qtgui_sink_x_0.enable_rf_freq(True)
+
+        self.top_layout.addWidget(self._qtgui_sink_x_0_win)
+        self.osmosdr_source_0 = osmosdr.source(
+            args="numchan=" + str(1) + " " + ""
+        )
+        self.osmosdr_source_0.set_time_unknown_pps(osmosdr.time_spec_t())
+        self.osmosdr_source_0.set_sample_rate(samp_rate)
+        self.osmosdr_source_0.set_center_freq(435e6, 0)
+        self.osmosdr_source_0.set_freq_corr(0, 0)
+        self.osmosdr_source_0.set_dc_offset_mode(0, 0)
+        self.osmosdr_source_0.set_iq_balance_mode(0, 0)
+        self.osmosdr_source_0.set_gain_mode(False, 0)
+        self.osmosdr_source_0.set_gain(10, 0)
+        self.osmosdr_source_0.set_if_gain(20, 0)
+        self.osmosdr_source_0.set_bb_gain(20, 0)
+        self.osmosdr_source_0.set_antenna('', 0)
+        self.osmosdr_source_0.set_bandwidth(0, 0)
+        self.low_pass_filter_0 = filter.fir_filter_ccf(
+            20,
+            firdes.low_pass(
+                1,
+                samp_rate,
+                75e3,
+                25e3,
+                window.WIN_HAMMING,
+                6.76))
+        self.dc_blocker_xx_0 = filter.dc_blocker_cc(1024, True)
+        self.audio_sink_0 = audio.sink(48000, '', True)
+        self.analog_wfm_rcv_0 = analog.wfm_rcv(
+        	quad_rate=480e3,
+        	audio_decimation=10,
+        )
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_wfm_rcv_0, 0), (self.audio_sink_0, 0))
+        self.connect((self.dc_blocker_xx_0, 0), (self.low_pass_filter_0, 0))
+        self.connect((self.dc_blocker_xx_0, 0), (self.qtgui_sink_x_0, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.rational_resampler_xxx_0, 0))
+        self.connect((self.osmosdr_source_0, 0), (self.dc_blocker_xx_0, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.analog_wfm_rcv_0, 0))
+
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "fmradio")
+        self.settings.setValue("geometry", self.saveGeometry())
+        self.stop()
+        self.wait()
+
+        event.accept()
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 75e3, 25e3, window.WIN_HAMMING, 6.76))
+        self.osmosdr_source_0.set_sample_rate(self.samp_rate)
+
+
+
+
+def main(top_block_cls=fmradio, options=None):
+
+    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+
+    tb.start()
+
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    qapp.exec_()
+
+if __name__ == '__main__':
+    main()
