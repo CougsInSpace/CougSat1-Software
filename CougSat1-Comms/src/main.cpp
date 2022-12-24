27f64a021d2bec80dff6552c6e5b0db4e07b2415
Trav798 <68037803+Trav798@users.noreply.github.com>
2022-12-23 19:43:56 -0800

    Added improved FM radio

--- CougSat1-Comms/src/main.cpp
+++ CougSat1-Comms/src/main.cpp
@@ -3,7 +3,7 @@
 #include <CISConsole.h>
 
 #include <Synth/ADF4360.h>
-#include "IQArrayFM.h"
+#include "IQArray.h"
 
 #include <Trig.h>
 
@@ -68,7 +68,7 @@ int main(void) {
     // statusLED = !statusLED;
     // LOG("Loop", "%d", index);
     // phase = (phase + 90) % 360;
-    for (int i = 0; i < 110/*138*360*/; ++i) { // 10e3 is .4 baud, 1e3 is around 11.9
+    for (int i = 0; i < 1/*138*360*/; ++i) { // 10e3 is .4 baud, 1e3 is around 11.9
       // uint16_t codeI = uint16_t((LUT_SINE[i % TRIG_LUT_SIZE] + 1) / 2 * 1023
       // + 0.5);
 
@@ -77,23 +77,23 @@ int main(void) {
 
       // Setup and hold times met due to 80MHz => 12.5ns > 3ns
       // uint16_t codeI = LUT_SINE_10b[(i + 0) % TRIG_LUT_SIZE]; // fast way to do sine. Max value of Look-Up Table is 1023, a 10 bit int
-      uint16_t codeI = LUT_SINE_10b[(IQArr[index] + 0) % TRIG_LUT_SIZE];
-      // uint16_t codeI = IQArr[index];
+      // uint16_t codeI = LUT_SINE_10b[(IQArr[index] + 0) % TRIG_LUT_SIZE];
+      uint16_t codeI = MSG_DAC_FM_I[index];
       dacClk         = 0x00;
       // The mask for PortOut dacBus is 1111111111001000. CodeI will be some 16 bit int representing sine. The last 6 bits of the mask are 0 with the exception of the one in the 4th position.
       // The line of code before compares some int codeI in the binary form xxxxxxxxxxxxxxx with an or operator. The mask takes the first 10 positions of that int and keeps them, throws out the
       // rest. The int is left shifted 6 to account for the first 6 places of the mask being 0 with the exception of the 4th, which is taken care of with the or 0x8 (the number 8, or 1000 in binary)
       dacBus         = (codeI << 6) | 0x8; // bit math, compare 8 in binary (1000) to whatever codeI << 6 is. starts as 16 bit, shifted over to 22(?) bit
       dacClk         = 0x10;
-      // wait_us(5);
+      wait_ns(9000);
 
       // uint16_t codeQ = LUT_SINE_10b[(i + TRIG_LUT_COSINE) % TRIG_LUT_SIZE];//fast wat to do cosine?
-      uint16_t codeQ = LUT_SINE_10b[(IQArr[index] + TRIG_LUT_COSINE) % TRIG_LUT_SIZE];
-      // uint16_t codeQ = IQArr[index];
+      // uint16_t codeQ = LUT_SINE_10b[(IQArr[index] + TRIG_LUT_COSINE) % TRIG_LUT_SIZE];
+      uint16_t codeQ = MSG_DAC_FM_Q[index];
       dacClk         = 0x00;
       dacBus         = (codeQ << 6) | 0x0;
       dacClk         = 0x10;
-      // wait_us(1);
+      wait_ns(9000);
       // 1/(10us*360) = 277Hz
     }
     // index = (index + 1) % dataLen;
